0c74503acc4905ac04eb0f33b0230570d92d50ce
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-12-08 12:03:32 +0400

    authentication

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Button>Click</Button>
-    </div>
-  );
+  useEffect(() => {
+    redirect("/api/auth/login?post_login_redirect_url=/dashboard");
+  }, []);
+  return <div></div>;
 }
