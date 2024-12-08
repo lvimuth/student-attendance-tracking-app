f5747c17edb57334a9efbd7358ca3559ec75d568
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-12-08 12:45:58 +0400

    header

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+"use client";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import Image from "next/image";
+import React from "react";
 
 function Header() {
+  const { user } = useKindeBrowserClient();
   return (
-    <div>Header</div>
-  )
+    <div className="p-4 shadow-sm border flex justify-between">
+      <div></div>
+      <div>
+        <Image
+          src={user?.picture}
+          width={35}
+          height={35}
+          alt=""
+          className="rounded-full"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
