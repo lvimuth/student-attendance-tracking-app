2377cae0faa9b1f20628a0ce86ade4ada01cf02f
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-12-08 11:41:36 +0400

    kinde

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello</h1>
+      <Button>Click</Button>
     </div>
   );
 }
